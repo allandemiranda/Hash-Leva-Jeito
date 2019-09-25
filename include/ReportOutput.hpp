447a5a1b9d89c4afe8850c2d0cfb15dddf2e8a62
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-09-25 15:53:09 -0300

    Report Output

--- include/ReportOutput.hpp
+++ include/ReportOutput.hpp
@@ -4,7 +4,25 @@
  * @brief Classe para gerenciar saída de arquivo de log
  * @version 0.1
  * @date 25-09-2019
- * 
+ *
  * @copyright Copyright (c) 2019
- * 
+ *
  */
+
+#ifndef REPORTOUTPUT_HPP_
+#define REPORTOUTPUT_HPP_
+
+#include <string>  // std::string
+#include <vector>  // std::vector
+#include "Log.hpp"
+
+class ReportOutput {
+ private:
+  void setFileOut(std::vector<Log>, std::string);
+
+ public:
+  ReportOutput(std::vector<Log>, std::string);
+  ~ReportOutput(void);
+};
+
+#endif
---
+++ src/ReportOutput.cpp
@@ -0,0 +1,53 @@
+/**
+ * @file ReportOutput.cpp
+ * @author Allan de Miranda Silva (allandemiranda@gmail.com)
+ * @brief Métodos para a classe ReportOutput
+ * @version 0.1
+ * @date 25-09-2019
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+
+#include "ReportOutput.hpp"
+#include <fstream>   // std::ifstream, std::ofstream
+#include <iostream>  // std::cerr
+#include <string>    // std::string
+#include <vector>    // std::vector
+#include "Log.hpp"
+
+/**
+ * @brief Construct a new Report Output:: Report Output object
+ *
+ * @param logs Vetor com os logs a ser salvo em um arquivo de texto
+ * @param path Caminho do arquivo para salvar
+ */
+ReportOutput::ReportOutput(std::vector<Log> logs, std::string path) {
+  setFileOut(logs, path);
+}
+
+/**
+ * @brief Destroy the Report Output:: Report Output object
+ *
+ */
+ReportOutput::~ReportOutput(void) {}
+
+/**
+ * @brief Set the File Out object
+ *
+ * @param logs Vetor com os logs a ser salvo em um arquivo de texto
+ * @param path Caminho do arquivo para salvar
+ */
+void setFileOut(std::vector<Log> logs, std::string path) {
+  try {
+    std::ofstream newFile;
+    std::string fileName = path;
+    newFile.open(fileName, std::ios::app);
+    for (auto i(0u); i < logs.size(); ++i) {
+      newFile << logs[i].getHash() << " " << logs[i].getFilePath() << std::endl;
+    }
+    newFile.close();
+  } catch (const std::ios_base::failure& e) {
+    std::cerr << e.what() << '\n';
+  }
+}

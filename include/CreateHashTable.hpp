6e86b6bc0c534f6aee3915201cc8731c0eb1fedb
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-09-25 16:43:19 -0300

    Create Hash Table

---
+++ include/CreateHashTable.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file CreateHashTable.hpp
+ * @author Allan de Miranda Silva (allandemiranda@gmail.com)
+ * @brief Classe para a criação do arquivo de configuração
+ * @version 0.1
+ * @date 25-09-2019
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+
+#ifndef CREATEHASHTABLE_HPP_
+#define CREATEHASHTABLE_HPP_
+
+#include <string>  // std::string
+#include <vector>  // std::vector
+#include "Log.hpp"
+
+class CreateHashTable {
+ private:
+  std::vector<Log> hash_table;
+  void setNewHashInTable(Log);
+
+ public:
+  std::vector<Log> getHashTable(void);
+  CreateHashTable(std::vector<std::string>);
+  ~CreateHashTable(void);
+};
+
+#endif
---
+++ src/CreateHashTable.cpp
@@ -0,0 +1,52 @@
+/**
+ * @file CreateHashTable.cpp
+ * @author Allan de Miranda Silva (allandemiranda@gmail.com)
+ * @brief Métodos para classe CreateHashTable
+ * @version 0.1
+ * @date 25-09-2019
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+
+#include "CreateHashTable.hpp"
+#include <string>  // std::string
+#include <vector>  // std::vector
+#include "HashFile.hpp"
+#include "Log.hpp"
+
+/**
+ * @brief Create a Hash Table:: Create Hash Table object
+ *
+ * @param path Vetor de caminho de arquivos
+ */
+CreateHashTable::CreateHashTable(std::vector<std::string> path) {
+  for (std::string path_file : path) {
+    HashFile newFile(path_file);
+    std::string md5_hash = newFile.getHash();
+    Log newLog(md5_hash, path_file);
+    hash_table.push_back(newLog);
+  }
+}
+
+/**
+ * @brief Create a Hash Table::~ Create Hash Table object
+ *
+ */
+CreateHashTable::~CreateHashTable(void) {}
+
+/**
+ * @brief Set the New Hash In Table object
+ *
+ * @param new_log Novo log
+ */
+void CreateHashTable::setNewHashInTable(Log new_log) {
+  hash_table.push_back(new_log);
+}
+
+/**
+ * @brief Get the Hash Table object
+ *
+ * @return std::vector<Log> Vetor de logs gerado
+ */
+std::vector<Log> CreateHashTable::getHashTable(void) { return hash_table; }

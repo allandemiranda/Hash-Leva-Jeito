b9b9cb378e80a5df42034743011bda8b70a397ff
Allan de Miranda Silva <allandemiranda@gmail.com>
2019-09-25 11:06:22 -0300

    main

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.cpp
+ * @author Allan de Miranda Silva (allandemiranda@gmail.com)
+ * @brief Main
+ * @version 0.1
+ * @date 25-09-2019
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
